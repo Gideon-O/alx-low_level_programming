8cdd8095abc4ade8315a329642b8e2fd14cd6328
Gideon-O <osorogideon10@gmail.com>
2022-03-10 02:51:47 -0800

    Puts command

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ * Prints a string
+ * main is the entry to program
+ * returns zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
