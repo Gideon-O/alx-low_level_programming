42b7cae548a86ed5f1300d2e340d9c2b3e3cd628
Gideon-O <osorogideon10@gmail.com>
2022-03-11 01:40:53 -0800

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,16 @@
 int main(void)
 {
 	int n;
+	int d;
 
 	for (n = '0'; n < 10; n++)
 	{
 		putchar(n);
 		if (d != '9')
 		{
-			putchar(",");
-			putchar(" ");
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar("\n");
+	putchar('\n');
 }
