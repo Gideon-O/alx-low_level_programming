81d71f7f863ee30d2d84853e7aa1f1ce40ce59e6
Gideon-O <osorogideon10@gmail.com>
2022-03-11 00:22:55 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	l_letter = 'z';
+
+	for (letter = 'a'; letter <= l_letter; letter++)
+	{
+		if (letter == 'e' || letter == 'q')
+			pass;
+		else
+		{
+			putchar("%c");
+			putchar("\n");
+		}
+	return (0);
