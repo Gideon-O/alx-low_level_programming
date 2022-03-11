caa1b8ecbd5a3f5c4cef2eb3f38ff6e61f7ba452
Gideon-O <osorogideon10@gmail.com>
2022-03-11 00:32:44 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		/* type conversion */
+		putchar("%c", (char)number);
+		putchar("\n");
+	}
+	return (0);
+}
