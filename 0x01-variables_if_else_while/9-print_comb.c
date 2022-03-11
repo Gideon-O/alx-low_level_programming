f21ded9578705cb6b21ce57d0660a4d75b7be34d
Gideon-O <osorogideon10@gmail.com>
2022-03-11 01:01:03 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n < 10; n++)
+	{
+		putchar(n);
+		if (d != '9')
+		{
+			putchar(",");
+			putchar(" ");
+		}
+	}
+	putchar("\n");
+}
