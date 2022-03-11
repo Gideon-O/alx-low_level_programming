eb3f7a4d087f54890cd1b74737270c5a924c7e3f
Gideon-O <osorogideon10@gmail.com>
2022-03-11 02:07:58 -0800

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nm1;
+	int nm2;
+
+	for (nm1 = '0'; nm1 <= '9'; nm1++)
+	{
+		for (nm2 = nm1 + 1; nm2 <= '9'; nm2++)
+		{
+			putchar(nm1);
+			putchar(nm2);
+
+			if (nm1 == '8' && nm2 == '9')
+			{}
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
