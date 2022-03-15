bcd5efafe01574ecde726230797f49465d292af4
Gideon-O <osorogideon10@gmail.com>
2022-03-15 10:29:23 -0700

    List minutes in 24hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,18 +8,18 @@
 
 void jack_bauer(void)
 {
-	int hr2 = 0, hr1;
+	int hr2 = '0', hr1;
 	int mn, sc;
 
-	while (hr2 < 3)
+	while (hr2 < '3')
 	{
-		hr1 = 0;
+		hr1 = '0';
 
-		while (hr1 < 4)
+		while (hr1 < '4')
 		{
-			for (mn = 0; mn < 6; mn++)
+			for (mn = '0'; mn < '6'; mn++)
 			{
-				for (sc = 0; sc <= 9; sc++)
+				for (sc = '0'; sc <= '9'; sc++)
 				{
 					_putchar(sc);
 				}
