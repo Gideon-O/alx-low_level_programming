ca6950617a03f43942333e8449fa47636bee28fa
Gideon-O <osorogideon10@gmail.com>
2022-03-15 09:22:32 -0700

    Absolute value returned

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Checks holberton
+ * @int: Parameter for the function _abs
+ *
+ * Description: Converts all numbers to their absolute values
+ *
+ * Return: Always absolute
+ */
+
+int _abs(int x)
+{
+	int absolute = 0;
+
+	absolute = abs(x);
+
+	return (absolute);
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entryp point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int r = 0;
+	int absolute = _abs(r);
+
+	_putchar(absolute);
+	_putchar('\n');
+
+	return (0);
+}
