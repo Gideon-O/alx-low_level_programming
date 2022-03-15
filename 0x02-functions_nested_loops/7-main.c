47693ae372c7a3972d7ad36d6f923adfa435e1d2
Gideon-O <osorogideon10@gmail.com>
2022-03-15 09:36:00 -0700

    Last digit of number

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int r = 0;
+
+	int last = print_last_digit(r);
+
+	_putchar(last);
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_last_digit - Check holberton
+ * @m: Parameter for the function
+ *
+ * Description: Uses the mudulus operator
+ *
+ * Return: Always x
+ */
+
+int print_last_digit(int m)
+{
+	int x = 0;
+
+	x = m % 10;
+
+	return (x);
+}
