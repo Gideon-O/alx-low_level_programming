38675d45a5b9b2fbf6b3fba3b74e18185382bc0a
Gideon_O <osorogideon10@gmail.com>
2022-03-16 16:30:08 +0300

    Fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	long x = 1, y = 2, z;
+
+	int this_many;
+
+	printf("%ld, ", x);
+
+	for (this_many = 0; this_many < 100; this_many++)
+	{
+		if (this_many <= 98)
+		{
+			printf("%ld", y);
+			z = x + y;
+			x = y;
+			y = z;
+			printf(", ");
+		}
+		if (this_many == 99)
+		{
+			printf("%ld", y);
+		}
+		
+	}
+
+	printf("\n");
+
+	return (0);
+}
