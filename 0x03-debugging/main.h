cbba69746177956e5295fec026c997e07c415642
Gideon_O <osorogideon10@gmail.com>
2022-03-16 14:38:06 +0300

    Calendar

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,3 @@
 int largest_number(int, int, int);
-int convert_day(int, int);
-int print_remaining_days(int, int, int);
+int convert_day(int month, int day);
+int print_remaining_days(int day, int month, int year);
