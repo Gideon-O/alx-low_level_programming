a78c734e9b2d3fff47b4ebde35acbf0ac9572d34
Gideon-O <osorogideon10@gmail.com>
2022-03-21 01:29:12 -0700

    Change value using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Changes the value of a variable being pointed to
+ * @n: The integer parameter argument for the function
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	int *x = &n;
+
+	*x = 98;
+}
