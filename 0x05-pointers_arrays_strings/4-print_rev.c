4d98d921721cc0f8708ce0d878d0eed62a4c9e6d
Gideon-O <osorogideon10@gmail.com>
2022-03-21 02:26:37 -0700

    Reverse a string array

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: A string argument
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int i = strlen(s);
+
+	for (; i >= 0; i--)
+	{
+		_putchar("%c", s[i]);
+	}
+	_putchar('\n');
+}
