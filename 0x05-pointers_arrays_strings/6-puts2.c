5c235e8f629a608c87ad583c4442f2babbca7d6b
Gideon-O <osorogideon10@gmail.com>
2022-03-21 08:15:12 -0700

    Print half the string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - Prints each character of a string on a new line
+ * @str: String to print its characters
+ * Return: Void
+ */
+
+void puts2(char *str)
+{
+	int i = strlen(str), x;
+
+	x = 0;
+	while (x < i)
+	{
+		if (x == 0 || x == 2 || x % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+
+		else
+		{
+			continue;
+		}
+		x++;
+	}
+	_putchar('\n');
+}
