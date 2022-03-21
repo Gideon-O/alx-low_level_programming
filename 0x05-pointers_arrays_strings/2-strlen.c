bd39949d61fe58d0e97721f90c873f6f13f74b75
Gideon-O <osorogideon10@gmail.com>
2022-03-21 02:06:53 -0700

    Lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Getr the lenght of aa string
+ * @s: Input aargument, a string
+ * Return: VOid
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = strlen(s);
+
+	return (i);
+}
