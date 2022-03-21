923d5af18034d7c58c32bdccae42a9d0ad39d73e
Gideon-O <osorogideon10@gmail.com>
2022-03-21 02:13:29 -0700

    Puts prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string to the standard output and
+ * adds a new line
+ * @str: Input parameter, must be a string
+ *
+ * Return: Void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
