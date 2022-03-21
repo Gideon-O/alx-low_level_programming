70912add27405c1e742d7ad92388f8cb310e8bb2
Gideon-O <osorogideon10@gmail.com>
2022-03-21 01:55:24 -0700

    Swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaphs the integer values of given variables
+ * @a: Parameter argument
+ * @b: Parameter argument
+ *
+ * Return: Void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = &b;
+	*b = *a;
+	*a = c;
+}
