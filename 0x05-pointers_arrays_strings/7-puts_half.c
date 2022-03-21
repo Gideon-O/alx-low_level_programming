6f179ca6c945893759a10fc05f50880ee6119824
Gideon-O <osorogideon10@gmail.com>
2022-03-21 08:31:20 -0700

    Half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - Prints a string from halfway to the end
+ * @str: String input
+ * Return: Void
+ */
+
+void puts_half(char *str)
+{
+	int i = strlen(str);
+	int x = 0;
+
+	for (x = 0; x < i; x++)
+	{
+		if (x < i / 2)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(str[x]);
+		}
+	}
+}
