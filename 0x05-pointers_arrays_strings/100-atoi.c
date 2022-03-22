964fea583d859d99deb23c4f3e31d0b9c60c50cc
Gideon-O <osorogideon10@gmail.com>
2022-03-21 22:27:09 -0700

    Print integers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _atoi - Converts any integer character to an integer
+ * @s: Character array
+ * Return: Integer value
+ */
+
+int _atoi(char *s)
+{
+	int x = 0;
+
+	for (; x < strlen(s); x++)
+	{
+		if (s[x] == '+')
+			_putchar('+');
+		else if (s[x] == '-')
+			_putchar('-');
+		if (isdigit(s[x]))
+		{
+			int y = atoi(s[x]);
+		}
+	}
+	return (y);
+}
