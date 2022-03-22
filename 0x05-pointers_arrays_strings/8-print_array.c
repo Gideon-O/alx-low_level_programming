de5697d8e7511605219f8f49347004c9dfff10ec
Gideon-O <osorogideon10@gmail.com>
2022-03-21 21:39:23 -0700

    Print integer arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - Prints the array passed to it
+ * @a: The array to print
+ * @n: The indexes to print
+ * Return: Void
+ */
+
+void print_array(int *a, int n)
+{
+	int x = 0;
+
+	for (; x < n; x++)
+	{
+		printf("%d", a[x]);
+
+		while (x != n - 2)
+		{
+			printf(", ");
+		}
+	}
+	printf('\n');
+}
