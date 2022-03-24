8d6401f854080ad80dbb0cb37318d318410303cb
Gideon-O <osorogideon10@gmail.com>
2022-03-23 23:05:52 -0700

    Concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Concatenates/appends an string to the end of another string
+ * @dest: String argument
+ * @src: String argument
+ *
+ * Return: A character array
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}
