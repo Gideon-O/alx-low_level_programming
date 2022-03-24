cca95f17f92fc3a5186033964c2242a812544480
Gideon-O <osorogideon10@gmail.com>
2022-03-24 01:30:33 -0700

    To uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,11 +9,12 @@
 
 char *string_toupper(char *s)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && [i] <= 'z')
+		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
 		i++;
 	}
