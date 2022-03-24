c63aa71b00ffc5f28281c4c3725204a571ed03fd
Gideon-O <osorogideon10@gmail.com>
2022-03-24 02:31:52 -0700

    Capitalize word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,36 +9,39 @@
 
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	if (str[0] >= 'a' && str[0] <= 'z')
+	while (str[i])
 	{
-		str[0] = str[0] - 32;
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		if (check_separator(str[i]) && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			str[i + 1] = str[i + 1] - 32;
+		i++;
 	}
 
-	for (i = 0; str[i] != '\0'; i++)
+}
+
+/**
+ * check_separator - Sepatarators of words
+ * space, tab, newline, ,, ., ;, !, ?, \", (, ), {, }
+ * @c: Input character
+ *
+ * Return: 1 if separator, 0 otherwise
+ */
+
+int check_separator(char c)
+{
+	int r = 0;
+
+	char separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+
+	for (; r < 13; r++)
 	{
-		switch (str[i])
-		{
-			case ',':
-			case ';':
-			case '.':
-			case '!':
-			case '?':
-			case '"':
-			case '(':
-			case ')':
-			case '{':
-			case '}':
-			case ' ':
-			case '\n':
-			case '\t':
-				if (str[i + 1] > 96 && str[i + 1] < 123)
-				{
-					str[i + 1] = str[i + 1] - 32;
-				}
+		if (s == separators[r])
+			return (1);
 	}
-	return (str);
+
+	return (0);
 }
