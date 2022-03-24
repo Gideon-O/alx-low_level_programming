d1677e96ac470f310108c079cc3a202f537b6e31
Gideon-O <osorogideon10@gmail.com>
2022-03-23 23:44:38 -0700

    Concatenate a string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,34 @@
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
-	
-	return (dest);
+	int i = 0, j = 0;
+	char *temp;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+
+	int tot = i + j;
+	int y = 0;
+
+	while (y < tot)
+	{
+		if (y < i)
+		{
+			temp[y] = dest[y];
+		}
+		else if (y > i)
+		{
+			int z = 0;
+
+			temt[y] = src[z];
+		}
+	}
+	return (temp);
 }
