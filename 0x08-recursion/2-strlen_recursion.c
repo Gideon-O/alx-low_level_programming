9859c6b7c5919e7156939c01d837f698db305807
Gideon-O <osorogideon10@gmail.com>
2022-03-30 11:54:22 -0700

    Counts string characters

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - gets the lenght of a string
+ * @s: string to get lenght
+ *
+ * Return: Integer value
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i + 1;
+		_strlen_recursion(s - 1);
+	}
+
+	return (i);
+}
