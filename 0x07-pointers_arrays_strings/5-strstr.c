8ef698fdc7477c69efcc3e0eee2dd10bfa3b587b
Gideon-O <osorogideon10@gmail.com>
2022-03-30 04:51:08 -0700

    Search for a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring in a string
+ * @haystack: mina string input
+ * @needle: substring to search for
+ *
+ * Return: character for pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *n = needle;
+	char *h = haystack;
+
+	while (*n)
+	{
+		n = needle;
+		h = haystack;
+		while (*haystack == *needle)
+		{
+			needle++;
+			haystack++;
+		}
+
+		if (needle == '\0')
+			return (haystack);
+
+		haystack = h + 1;
+	}
+	return (NULL);
+}
