ac122983eb6b22ddbceeea8d595800dee9b33044
Gideon-O <osorogideon10@gmail.com>
2022-03-30 08:21:32 -0700

    Search for a character, return address

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - searches for a character in a string, returns a pointer
+ * @s: String array
+ * @c: Character to be searched
+ *
+ * Return: Character (pointer address)
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (c == *s)
+			return (s);
+
+		s++;
+	}
+
+	if (c == *s)
+		return (s);
+
+	return (NULL);
+}
