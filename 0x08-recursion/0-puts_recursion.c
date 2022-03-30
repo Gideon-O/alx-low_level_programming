396305af607e238c6cd827b11ba48dac49d45af3
Gideon-O <osorogideon10@gmail.com>
2022-03-30 10:55:06 -0700

    Prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a character by recursion
+ * @s: String to be printed
+ *
+ * Return: Void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(s);
+		_puts_recursion(s++);
+	}
+
+	else
+		_putchar('\n');
+}
