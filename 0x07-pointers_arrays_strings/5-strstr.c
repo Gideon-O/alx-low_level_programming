d198cbed4946a8d3692f87f227ae88e0f9098c6f
Gideon-O <osorogideon10@gmail.com>
2022-03-30 05:07:58 -0700

    Search for substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -24,12 +24,13 @@ char *_strstr(char *haystack, char *needle)
 				n++;
 				h++;
 			}
-			
-			if (needle == '\0')
-				return (haystack);
-			
-			haystack = h + 1;
+			else
+				break;
 		}
+
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
 	}
 	return (NULL);
 }
