e407376db9ca896a42d75274b876630c8312de80
Gideon-O <osorogideon10@gmail.com>
2022-04-01 07:51:46 -0700

    Initial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: Argument count
+ * @argv: Argument array
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%d", argc);
+	}
+
+	return (0);
+}
