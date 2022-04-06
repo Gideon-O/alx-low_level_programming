d66fa5da7f00db5c2639a6d0ed098de08eb1173a
Gideon-O <osorogideon10@gmail.com>
2022-04-05 20:31:01 -0700

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * free_grid - free an allocated memory block
+ * @grid: memory block to deallocate
+ * @height: height of grid
+ *
+ * Return: Void
+ */
+
+void (int **grid, int height)
+{
+	int m;
+	return;
+}
