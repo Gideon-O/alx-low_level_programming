9d72e7b8b4b46a82136e6d941b21cb45709035e1
Gideon-O <osorogideon10@gmail.com>
2022-04-13 12:00:32 -0700

    String concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,36 +10,44 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *new_str;
-	int y, x = 0, i = 0;
+	char *new_str, starts1, starts2;
+	int i = 0, lens1, lens2;
 
+	starts1 = s1;
+	starts2 = s2;
+	if (s1 == NULL)
+		s1 = "";
 	while (*s1)
 	{
-		x++;
+		lens1++;
 		s1++;
 	}
-
+	s1 = starts1;
+	if (s2 == NULL)
+		s2 = "";
 	while (*s2)
 	{
-		i++;
+		lens2++;
 		s2++;
 	}
-
-	for (y = 0; y < x; y++)
+	s2 = starts2;
+	new_str = malloc(sizeof(char) * (lens2 + lens2 + 1));
+	starts1 = new_str;
+	if (new_str == NULL)
+		return (NULL);
+	for (; i < (lens1 + lens2); i++)
 	{
-		new_str[y] = s1[y];
+		if (i < lens1)
+		{
+			new_str[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[i] = *s2;
+			s2++;
+		}
 	}
-
-	for (; y < x + i; y++)
-	{
-		int m = 0;
-
-		new_str[y] = s2[m];
-		m++;
-	}
-
-	if (y == x + i)
-		new_str[y] = '\0';
-
-	return (new_str);
+	new_str[i] = '\0';
+	return (starts1);
 }
