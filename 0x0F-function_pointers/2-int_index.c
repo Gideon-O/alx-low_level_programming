7f349495c3dcd179d861dfb90755fe872db205f3
Gideon-O <osorogideon10@gmail.com>
2022-04-15 04:26:32 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,11 +17,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (cmp == NULL || array == NULL)
 		return (-1);
+
 	if (size <= 0)
 		return (-1);
+
 	for (; i < size; i++)
+	{
 		if (cmp(array[i]))
 			return (i);
+	}
+
 	if (i == size)
 		return (-1);
 	return (-1);
