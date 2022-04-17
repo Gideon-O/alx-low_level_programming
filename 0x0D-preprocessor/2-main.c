76174c73d146821e0d31c2400dd2820981d62dc9
Gideon-O <osorogideon10@gmail.com>
2022-04-17 05:30:16 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point to the program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *file_name = _FILE_;
+
+	printf("%s\n", file_name);
+	return (0);
+}
