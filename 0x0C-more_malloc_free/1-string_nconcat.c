ee2c20638dfb48395d75431ee607aa3ca1e78e46
Gideon-O <osorogideon10@gmail.com>
2022-04-17 07:56:59 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,13 +18,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 
-	while (*s1)
+	while (s1[lens1])
 		lens1++;
 
 	if (s2 == NULL)
 		s2 = "";
 
-	while (*s2)
+	while (s2[lens2])
 		lens2++;
 
 	if (n >= lens2)
