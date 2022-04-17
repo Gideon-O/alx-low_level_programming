e5b48561c1178dcfefd258bf4c7e7a32d78ff471
Gideon-O <osorogideon10@gmail.com>
2022-04-17 06:54:59 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory
+ * @b: Number of elements
+ *
+ * Return: Pointer on success, or exit with error 98
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	void *s;
+
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+	return (s);
+}
