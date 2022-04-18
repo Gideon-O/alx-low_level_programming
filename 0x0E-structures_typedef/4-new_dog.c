af2fe7d4c52a150fe36172ad77902460b33e3382
Gideon-O <osorogideon10@gmail.com>
2022-04-18 00:47:09 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - A function that creates a new dog variable
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the dog variable
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+
+	my_dog->name;
+	my_dog->age;
+	my_dog->owner;
+
+	return (my_dog);
+}
