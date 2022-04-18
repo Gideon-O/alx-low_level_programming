a39b6a9b716acade80a377bc3aa1a413619f5075
Gideon-O <osorogideon10@gmail.com>
2022-04-18 00:28:09 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,11 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef dog dog_t;
+
+void (struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/* dog_t *new_dog(char *name, float age, char *owner); */
+dog_t *new_dog(char *name, float age, char *owner);
 /* void free_dog(dog_t *d); */
 
 #endif
