684c37686edde05a08c9001029fa5aae1dc90f87
Gideon-O <osorogideon10@gmail.com>
2022-04-18 01:06:27 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,10 +14,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
+	char *copy_name, *copy_owner;
+	int name_len, owner_len, i = 0;
 
-	my_dog->name;
-	my_dog->age;
-	my_dog->owner;
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	while (name[name_len])
+		name_len++;
+
+	while (owner[onwer_len])
+		owner_len++;
+	my_dog = malloc(sizeof(dog_t));
+	
+	if (my_dog == NULL)
+		return (NULL);
+
+	copy_name = malloc(name_len + 1);
+	for (i = 0; i < name_len; i++)
+		copy_name[i] = name[i];
+	copy_name[i] = '\0';
+
+	for (i = 0; i < owner_len; i++)
+		copy_owner[i] = owner[i];
+	copy_owner[i] = '\0';
+
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
 
 	return (my_dog);
 }
