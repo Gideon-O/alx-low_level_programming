06ce1e13ad869fd9d23674ad733717e36db40659
Gideon-O <osorogideon10@gmail.com>
2022-04-17 23:35:25 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for dog
+ * @name: Name of dog
+ * @age: Age of dogs
+ * @owner: Name of dog owner
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
