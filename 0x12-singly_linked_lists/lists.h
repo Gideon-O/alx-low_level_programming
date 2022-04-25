81e12421ca4e62a972f275bc80ec764fa70c824a
Gideon-O <osorogideon10@gmail.com>
2022-04-25 12:17:50 -0700

    Added struct list_t

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+/**
+ * struct list_t - Structure for list node
+ * @list_words: String
+ * @word_len: Lenght of the string
+ * @ptr: Pointer to the node
+ */
+
+struct list_t
+{
+	char *list_words;
+	int word_len;
+	struct list_t *ptr
+};
+
 size_t print_lists(const list_t *h);
 size_t list_len(const list *h);
 list_t *add_node(list_t **head, const char *str);
