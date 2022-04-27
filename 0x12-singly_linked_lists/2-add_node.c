c49b3569d1934ccf3102269bbc784ac480baaa21
Gideon-O <osorogideon10@gmail.com>
2022-04-27 10:19:02 -0700

    Add node at start

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - A function to add a node to a list ad head
+ * @head: Start of the string
+ * @str: String to be added to the head
+ *
+ * Return: Address of the list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	int length = 0;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+
+	while (str[length])
+		length++;
+
+	temp->len = length;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}
