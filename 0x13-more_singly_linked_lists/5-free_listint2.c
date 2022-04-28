ecdde22bb33e635137a1cb9fac749f55cae1780c
Gideon-O <osorogideon10@gmail.com>
2022-04-28 12:40:54 -0700

    Update lists.h

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,16 +10,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *temp;
+	listint_t *temp;
 
 	if (*head == NULL)
 		return;
 
-	current = *head;
-	while (current != NULL)
+	temp = *head;
+	while (*head)
 	{
-		temp = current;
-		current = current->next;
+		temp = *head;
+		*head = *head->next;
 		free(temp);
 	}
 	*head = NULL;
