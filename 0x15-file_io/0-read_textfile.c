2ebfbac2e69d702269fab296da0f1b7c0a14a504
Gideon-O <osorogideon10@gmail.com>
2022-05-05 11:21:18 -0700

    Read and write file with POSIX

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -34,8 +34,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	res_write = write(fd, buf, letters);
-	if (res_write == -1 || res_write != res_read)
+	res_write = write(STDOUT_FILENO, buf, res_read);
+	if (res_write == -1 || res_read != res_write)
 	{
 		free(buf);
 		return (0);
