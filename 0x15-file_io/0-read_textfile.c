b636be71fe2b19d61b5ea9caeb026d3a27bfa0eb
Gideon-O <osorogideon10@gmail.com>
2022-05-05 11:07:52 -0700

    Read and write a file in POSIX

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - A function that reads teh contents of a text file
+ * @filename: Name of the file to be read
+ * @letters: Number of letters to read and print
+ *
+ * Return: Number of readable and printable letters or 0 if file is NULL
+ * of unreadable
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, res_read, res_write;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	but = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	res_read = read(fd, buf, letters);
+	if (res_read == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	res_write = write(fd, buf, letters);
+	if (res_write == -1 || res_write != res_read)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+
+	return (res_write);
+}
