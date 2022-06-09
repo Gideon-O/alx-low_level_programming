29fd117affb00ffa5ce42c676d9e210b9f4fd5f9
Gideon-O <osorogideon10@gmail.com>
2022-06-09 11:22:21 -0700

    Print list values

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print list values
+ * @h: Pointer to list header
+ *
+ * Return: Always 0
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num = 0;
+	const temp = h;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		num++;
+	}
+	return (num);
+}
